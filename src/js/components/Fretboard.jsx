0df3b3530441f49674d0f4c8c016a6973b5009c4
Matthew Elmquist <mattelmquist@gmail.com>
2017-11-20 11:50:11 -0500

    project scaffold

--- src/js/components/Fretboard.jsx
+++ src/js/components/Fretboard.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-// import '../css/style.css';
-import '../styles/main.scss'
+import '../../styles/main.scss'
 
-import Fretboard from './components/Fretboard';
-import Strings from './components/Strings';
-
-export default class App extends Component {
+export default class Fretboard extends Component {
     render() {
         return (
-            <div className="app-bg-div">
+            <div className="fb-bg-div">
 
-                <Fretboard />
-                {/* <div className="string-container red-border">
+                <div className="string-container red-border">
                     <div className="fret-container blue-border">
                         <div className="fret-inner-div">
                             <div className="fret-circle"></div>
@@ -35,15 +30,12 @@ export default class App extends Component {
                 <div className="string-container red-border"></div>
                 <div className="string-container red-border"></div>
                 <div className="string-container red-border"></div>
-                <div className="string-container red-border"></div> */}
+                <div className="string-container red-border"></div>
                 
             </div>
         )
     }
 }
-
-render(<App />, document.getElementById('app'));
-
 
 /*
 
